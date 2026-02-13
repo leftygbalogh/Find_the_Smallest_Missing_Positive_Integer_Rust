f69cc193070a4091175016594e1fba14e15fb515
lefty <geza.balogh@trayport.com>
2026-02-13 16:05:48 +0100

    Babysteps to follow the concept to use a set for deduping and ordering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::collections::HashSet;
+use std::io::{self, BufRead};
+
+
+
+/*
+ * Complete the 'findSmallestMissingPositive' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY orderNumbers as parameter.
+ */
+
+fn findSmallestMissingPositive(orderNumbers: &[i32]) -> i32 {
+	// Write your code here
+	println!("SmallestMissingPositive orderNumbers: {:?}", &orderNumbers);
+	let mut set: HashSet<&i32> = HashSet::new();
+	for num in orderNumbers {
+		set.insert(&num);
+	}
+	println!("{:?}", set);
+	42
+
+}
+
 fn main() {
-    println!("Hello, world!");
+	let stdin = io::stdin();
+	let mut stdin_iterator = stdin.lock().lines();
+
+	let orderNumbers_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+	let mut orderNumbers: Vec<i32> = Vec::with_capacity(orderNumbers_count as usize);
+
+	for _ in 0..orderNumbers_count {
+		let orderNumbers_item = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+		orderNumbers.push(orderNumbers_item);
+	}
+
+	let result = findSmallestMissingPositive(&orderNumbers);
+
+	println!("{}", result);
 }
